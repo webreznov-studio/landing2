89e99bc34e4572af1616d39f72bbd5958c559c3a
webreznov-studio <73492632+webreznov-studio@users.noreply.github.com>
2021-06-10 18:16:37 +0300

    add yandex tag, add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Webreznov - web developer`,
+    description: `Разработка сайтов, email писем и веб приложений. Создаем удобные и эффективные сайты. Адаптируем верстку под разные устройства и браузеры. Создание электронных писем с фирменным стилем.`,
+    author: `@webreznov`,
+    siteUrl: `https://webreznov.ru/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -35,5 +36,11 @@ module.exports = {
         anonymize: true
       },
     },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        excludes: ['/index-admin']
+      }
+    }
   ],
 }
